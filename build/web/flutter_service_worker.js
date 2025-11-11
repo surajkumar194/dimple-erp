a60a1d95a6c7906ca16585f0fc78f29bbb4a55b7
suraj kumar <suraj1906155@gmail.com>
2025-11-11 14:26:09 +0530

    fixed base href for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "61be510373a02ed34473d884bd08c2ef
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2f9dc230255a2b6b53a21ca49b699a31",
+"flutter_bootstrap.js": "52bd639e4f6330e9cb7d0e4d8a576168",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
